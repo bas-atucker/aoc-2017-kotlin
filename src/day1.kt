5ac67bfd67873cdb3c49a3c7814b26d6f2d84f83
Adam Tucker <adam.tucker@bigasssolutions.com>
2017-12-01 09:06:00 -0500

    Clean up day 1

--- src/day1.kt
+++ src/day1.kt
@@ -9,7 +9,7 @@ class Day1 {
         fun part1() {
             val matchedDigitsSum = findSumOfMatchingDigitsWithIndexOffset(getInput(), 1)
 
-            println("$matchedDigitsSum is the sum of digits that matched.")
+            println("$matchedDigitsSum is the sum of digits that matched the next digit.")
         }
 
         fun part2() {
@@ -17,19 +17,17 @@ class Day1 {
             val halfListLength = input.size / 2
             val matchedDigitsSum = findSumOfMatchingDigitsWithIndexOffset(input, halfListLength)
 
-            println("$matchedDigitsSum is the sum of digits that matched.")
+            println("$matchedDigitsSum is the sum of digits that matched the digit halfway around the circular list.")
         }
 
         private fun findSumOfMatchingDigitsWithIndexOffset(input: CharArray, offset: Int): Int {
-            var matchedDigitsSum = 0
-
-            input.forEachIndexed { index, i ->
-                if (i == input[(index + offset) % input.size]) {
-                    matchedDigitsSum += (i.toInt() - '0'.toInt())
+            return input.foldIndexed(0) { index, sum, digit ->
+                if (digit == input[(index + offset) % input.size]) {
+                    sum + digit.toString().toInt()
+                } else {
+                    sum
                 }
             }
-
-            return matchedDigitsSum
         }
     }
 }
